66cf368f5396ad706759669d9a164ffb9e6c5d7a
Gevher Serra <serraseferoglu@gmail.com>
2025-02-23 17:28:38 +0100

    2nd commmit

--- src/java.java
+++ src/java.java
@@ -1,6 +1,9 @@
 public class java {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
+        int age;
+        age = 18;
+        System.out.println(age);
 
     }
 
